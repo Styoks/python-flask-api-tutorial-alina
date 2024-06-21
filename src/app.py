a6683b82e822e4bb1beba2a760aef86bbfeee847
Styoks <113370103+Styoks@users.noreply.github.com>
2024-06-21 18:30:16 +0000

    try

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
+
+@app.route('/todos', methods= ['GET'])
+
+def hello_world():
+    return "<h1>Hello!</h1>"
