fb05de2455d738271f50e77f3bbe7024e0494227
Styoks <113370103+Styoks@users.noreply.github.com>
2024-07-02 15:37:15 +0000

    Final

--- src/app.py
+++ src/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return todos
+
+@app.route('/todos/<int:Number>')
+
+def remove_todo(Number):
+    del todos[Number]
+    return todos
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
-
-@app.route('/todos', methods= ['GET'])
-
-def hello_world():
-    return "<h1>Hello!</h1>"
